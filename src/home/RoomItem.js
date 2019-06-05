7b47c14d67f631c713c46cfad66a0ef075d21cd3
DannySuarez <suarezd10@gmail.com>
2019-06-05 16:25:33 -0700

    added a button to remove individual chats

--- src/home/RoomItem.js
+++ src/home/RoomItem.js
@@ -1,12 +1,30 @@
 import Component from '../Component.js';
+import { chatsRef } from '../services/firebase.js';
 
 class RoomItem extends Component {
+
+    render() {
+        const dom = this.renderDOM();
+        const button = dom.querySelector('button');
+
+        const chatRoom = this.props.chat;
+        const chatRef = chatsRef.child(chatRoom.id);
+
+        button.addEventListener('click', () => {
+            chatRef.remove();
+
+        });
+
+        return dom;
+    }
+
     renderTemplate() {
-        const chats = this.props.chats;
+        const chat = this.props.chat;
 
         return /*html*/`
         <li class="chat-room">
-            <p>${chats.title}</p>
+            <a href="./chat.html?key=${chat.id}">${chat.title}</a>
+            <button>✗</button>
         </li>
         `;
     }
