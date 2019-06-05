be033d2509ed2c68faf3c1c2f30adf8137bbf08d
DannySuarez <suarezd10@gmail.com>
2019-06-05 11:45:45 -0700

    created a room list component

---
+++ src/home/RoomList.js
@@ -0,0 +1,11 @@
+import Component from '../Component.js';
+
+class RoomList extends Component {
+    renderTemplate() {
+        return /*html*/`
+            <ul class="chats">
+            </ul>
+        `;
+    }
+}
+export default RoomList;
