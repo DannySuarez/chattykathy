2f04da7c6e4d92c940eb4f6e5db41591f6890d38
DannySuarez <suarezd10@gmail.com>
2019-06-05 14:12:22 -0700

    rendered RoomItem in RoomList component

--- src/home/RoomList.js
+++ src/home/RoomList.js
@@ -1,6 +1,16 @@
 import Component from '../Component.js';
+import RoomItem from './RoomItem.js';
 
 class RoomList extends Component {
+    render() {
+        const list = this.renderDOM();
+        
+        const roomItem = new RoomItem();
+        list.appendChild(roomItem.render());
+
+        return list;
+    }
+
     renderTemplate() {
         return /*html*/`
             <ul class="chats">
@@ -9,3 +19,5 @@ class RoomList extends Component {
     }
 }
 export default RoomList;
+//bring in props
+//foreach on chats props
