9314bec2aa9faf18af6ccb17db2f86c7d0c97549
DannySuarez <suarezd10@gmail.com>
2019-06-05 14:35:00 -0700

    broguth in props and interpolated data from firebase

--- src/home/RoomItem.js
+++ src/home/RoomItem.js
@@ -2,9 +2,11 @@ import Component from '../Component.js';
 
 class RoomItem extends Component {
     renderTemplate() {
+        const chats = this.props.chats;
+
         return /*html*/`
         <li class="chat-room">
-            <h2>room 1</h2>
+            <h2>${chats.title}</h2>
         </li>
         `;
     }
