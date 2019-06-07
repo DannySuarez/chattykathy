c446d1a220351d8353a4b72c236d7c7a6fb2e405
DannySuarez <suarezd10@gmail.com>
2019-06-07 11:16:18 -0700

    users can only remove their own chat rooms

--- src/home/RoomItem.js
+++ src/home/RoomItem.js
@@ -1,30 +1,27 @@
 import Component from '../Component.js';
-import { chatsRef } from '../services/firebase.js';
+import { auth } from '../services/firebase.js';
 
 class RoomItem extends Component {
 
     render() {
         const dom = this.renderDOM();
-        const button = dom.querySelector('button');
-
-        const chatRoom = this.props.chat;
-        const chatRef = chatsRef.child(chatRoom.id);
-
+        const onRemove = this.props.onRemove;
+        const button = dom.querySelector('#button');
+        
         button.addEventListener('click', () => {
-            chatRef.remove();
-
+            onRemove(this.props.chat);
+            
         });
-
         return dom;
     }
-
+    
     renderTemplate() {
         const chat = this.props.chat;
 
         return /*html*/`
         <li class="chat-room">
             <a href="./chat.html?key=${chat.id}">${chat.title}</a>
-            <button>✗</button>
+            <button class="${auth.currentUser.uid === chat.owner ? '' : 'disabled'}" id="button">✗</button>
         </li>
         `;
     }
--- src/home/RoomList.js
+++ src/home/RoomList.js
@@ -1,13 +1,19 @@
 import Component from '../Component.js';
 import RoomItem from './RoomItem.js';
+import { chatsRef } from '../services/firebase.js';
 
 class RoomList extends Component {
     render() {
-        const chats = this.props.chats;
         const list = this.renderDOM();
+        const chats = this.props.chats;
         
         chats.forEach(chat => {
-            const roomItem = new RoomItem({ chat });
+            const roomItem = new RoomItem({ 
+                chat,
+                onRemove:(chat => {
+                    chatsRef.child(chat.key).remove();
+                })
+            });
             list.appendChild(roomItem.render());
         });
 
