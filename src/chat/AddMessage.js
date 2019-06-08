7a7c823d9b2b34f5540dd4f43c512fd741d0e2d7
DannySuarez <suarezd10@gmail.com>
2019-06-07 17:30:48 -0700

    addmessage component writes to firebase

--- src/chat/AddMessage.js
+++ src/chat/AddMessage.js
@@ -1,11 +1,39 @@
 import Component from '../Component.js';
-
+import { auth, messageRef } from '../services/firebase.js';
 class AddMessage extends Component {
+
+    render() {
+        const form = this.renderDOM();
+        
+        form.addEventListener('submit', event => {
+            event.preventDefault();
+            
+            const avatar = auth.currentUser.photoURL || './assets/default-avatar.png';
+            const userMessageRef = messageRef.push();
+            const formData = new FormData(form);
+
+            const message = {
+                message: formData.get('message'),
+                owner: auth.currentUser.uid,
+                displayName: auth.currentUser.displayName,
+                photoURL: avatar,
+                date: new Date().toISOString()
+            };
+
+            userMessageRef.set(message).then(() => {
+                form.reset();
+            });
+
+        });
+
+        return form;
+    }
+
     renderTemplate() {
         return /*html*/`
             <form class="add-message">
                 <input type="text" name="message" placeholder="Say Something">
-                
+                <button>Send</button>
             </form>
         `;
     }
