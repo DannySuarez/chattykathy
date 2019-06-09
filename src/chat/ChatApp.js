183a8a76904dc96aa20c90fd1f09f27d73e135b7
DannySuarez <suarezd10@gmail.com>
2019-06-09 15:48:51 -0700

    ChatApp now passes props

--- src/chat/ChatApp.js
+++ src/chat/ChatApp.js
@@ -1,6 +1,8 @@
 import Component from '../Component.js';
 import Header from '../shared/Header.js';
 import AddMessage from '../chat/AddMessage.js';
+import ChatRoom from '../chat/ChatRoom.js';
+import { messageRef } from '../services/firebase.js';
 
 class ChatApp extends Component {
 
@@ -13,6 +15,15 @@ class ChatApp extends Component {
 
         const addMessage = new AddMessage();
         dom.appendChild(addMessage.render());
+
+        messageRef.on('value', snapshot => {
+            const value = snapshot.val();
+            const message = value ? Object.values(value) : [];
+            chatRoom.update({ message });
+        });
+
+        const chatRoom = new ChatRoom({ message: [] });
+        dom.appendChild(chatRoom.render());
 
         return dom;
     }
