c03601241b39ade807ced2767799ff1196a45c7e
DannySuarez <suarezd10@gmail.com>
2019-06-09 15:48:28 -0700

    imported chatitem component.

--- src/chat/ChatRoom.js
+++ src/chat/ChatRoom.js
@@ -1,23 +1,24 @@
 import Component from '../Component.js';
-import Header from '../shared/Header.js';
-// import AddMessage from '../chat/AddMessage.js';
-
+import ChatItem from './ChatItem.js';
 
 class ChatRoom extends Component {
-
     render() {
         const dom = this.renderDOM();
-        
-        const header = new Header();
-        dom.insertBefore(header.render());
+        const message = this.props.message;
+
+        message.forEach(message => {
+            const chatItem = new ChatItem({ message });
+            dom.appendChild(chatItem.render());
+        });
+
 
         return dom;
     }
+
     renderTemplate() {
         return /*html*/`
-            <ul class="messages-container">
-                <li>this would be a list of messages</li>
-            </ul>
+            <div>
+            </div>
         `;
     }
 }
